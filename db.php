61b9851658e987d6698c3f01288b6c01cf6156be
sanjeevprabhakar <sanjeevprabhakar2021@gmail.com>
2022-12-04 20:45:18 -0700

    assignment5 update

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'sanjeev123');
+   define('DB_PASSWORD', 'sanjeev123!@#');
+   define('DB_DATABASE', 'assignment5');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+  
+?>
--- login.php
+++ login.php
@@ -12,11 +12,12 @@
     <div class="center">
         <h1> LOGIN </h1>
 
-        <form  method="post">
+        <form method="post">
             Username: <input type="text" name="username"> <br> <br>
             Password: <input type="password" name="password"> <br> <br>
-            <input type="submit" name ="submit" value="LOGIN">
-            <p> Not registered . <a href="register.php"> Click here to register</a> </p>
+            <input type="submit" name="submit" value="LOGIN">
+            <p> Not registered . <a href="register.php"> Click here to register</a> </p> <br> <br>
+            <p class = "status"> <?php  echo $errorMessage  ?>  </P>
         </form>
     </div>
 </body>
@@ -29,33 +30,46 @@
 
 if (isset($_POST["submit"])) {
 
-    include ("db.php");
+    include("db.php");
+    $erroeMessage = "";
 
     $username = $_POST["username"];
     $userPassword = $_POST["password"];
     $hashedUserPassword = password_hash($userPassword, PASSWORD_DEFAULT);
-
+    
     session_start();
 
-    $query = "SELECT passwordHash FROM  users WHERE username = '$username' ;";
-    
-    echo $query;
 
+    $query = "SELECT passwordHash FROM  users WHERE username='" . $username . "' ;";
+
+
+    echo $query;
+try {
+    //code...
     $result = mysqli_query($db, $query);
-    echo $result;
+    
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
     echo $row;
-   
+    $isPasswordRight = password_verify($userPassword, $result);
+    echo $isPasswordRight;
+
     $active = $row['active'];
+   // echo $active;
     $count = mysqli_num_rows($result);
-    if ($count == 1) {
-       
+    if ($count == 1 && $isPasswordRight == "success") {
+        echo "success";
         $_SESSION['login_user'] = $username;
 
-      //  header("location: profile.php");
+          header("location: profile.php");
     } else {
         $error = "Your Login Name or Password is invalid";
+        $errorMessage =$error;
     }
+} catch (\Throwable $th) {
+    //throw $th;
+    echo $th;
+}
+    
 } else {
 
     echo "form in progress";
